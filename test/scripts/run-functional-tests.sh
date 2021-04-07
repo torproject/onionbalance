aeec1e9ab7972ee957893c2e8e4a05be345f53dd
Felipe Jin <felipejinli2001@gmail.com>
2021-04-07 14:22:43 +0100

    Added functional_v3 tests to CI pipeline

--- test/scripts/run-functional-tests.sh
+++ test/scripts/run-functional-tests.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -ex
 
-[[ $TEST ~= functional_(.*) ]]
+[[ $TEST =~ functional_(.*) ]]
 version="${BASH_REMATCH[1]}"
+pwd
 pytest --cov-report=term-missing --cov=onionbalance test/functional/$version/
 pylint --disable=R,C,W onionbalance 
 flake8 onionbalance
