4ae8aa9a275c4b1ef0c7363a4a49fe56f7ec06c4
Silvio Rhatto <rhatto@torproject.org>
2025-04-07 11:22:40 -0300

    Fix: coding style (flake8) (#46)

--- onionbalance/__init__.py
+++ onionbalance/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-__author__  = "Silvio Rhatto, George Kadianakis, Donncha O'Cearbhaill"
+__author__ = "Silvio Rhatto, George Kadianakis, Donncha O'Cearbhaill"
 __contact__ = "rhatto@torproject.org"
-__url__     = "https://gitlab.torproject.org/tpo/onion-services/onionbalance/"
+__url__ = "https://gitlab.torproject.org/tpo/onion-services/onionbalance/"
 __license__ = "GPL"
 
 from onionbalance._version import get_versions
